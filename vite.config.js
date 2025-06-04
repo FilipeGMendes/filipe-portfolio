9e7aec66191023159b45302e8123b30c347dc87a
Filipe Mendes <filipe0505mendes@gmail.com>
2025-06-04 17:48:49 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-    base: '/filipe-portfolio',
-  plugins: [
-      react(),
-    tailwindcss(),
-  ],
+    base: '/filipe-portfolio/',
+  plugins: [react(), tailwindcss()],
 })
